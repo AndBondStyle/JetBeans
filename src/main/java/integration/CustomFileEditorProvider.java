5c3be4b23478ae7dca4bcd2ed1b13ceb9b0acfcc
AndBondStyle <andbondstyle@gmail.com>
2020-04-15 00:31:09 +0300

    Bugfix: custom editor not showing on IDE restart

--- src/main/java/integration/CustomFileEditorProvider.java
+++ src/main/java/integration/CustomFileEditorProvider.java
@@ -3,11 +3,12 @@ package integration;
 import com.intellij.openapi.fileEditor.FileEditor;
 import com.intellij.openapi.fileEditor.FileEditorPolicy;
 import com.intellij.openapi.fileEditor.FileEditorProvider;
+import com.intellij.openapi.project.DumbAware;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFile;
 import org.jetbrains.annotations.NotNull;
 
-public class CustomFileEditorProvider implements FileEditorProvider {
+public class CustomFileEditorProvider implements FileEditorProvider, DumbAware {
     @Override
     public boolean accept(@NotNull Project project, @NotNull VirtualFile file) {
         return file.getFileType() == CustomFileType.INSTANCE;
