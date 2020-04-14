91df31a0243826a3bb404490769fa4a885af4785
AndBondStyle <andbondstyle@gmail.com>
2020-04-15 00:31:54 +0300

    Bugfix: custom new file action without extension

--- src/main/java/integration/CustomNewFileAction.java
+++ src/main/java/integration/CustomNewFileAction.java
@@ -1,52 +1,16 @@
 package integration;
 
-import com.intellij.ide.IdeBundle;
 import com.intellij.ide.actions.CreateFileAction;
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.ui.Messages;
-import com.intellij.psi.PsiDirectory;
-import com.intellij.psi.PsiElement;
-import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 public class CustomNewFileAction extends CreateFileAction {
     CustomNewFileAction() {
         super("JetBeans Component", "Create new JetBeans component", CustomFileType.ICON);
     }
 
-    class Validator extends MyValidator {
-        private final String EXT = "." + CustomFileType.INSTANCE.getDefaultExtension();
-
-        public Validator(Project project, PsiDirectory directory) {
-            super(project, directory);
-        }
-
-        private String forceExtension(String input) {
-            input = input.trim();
-            if (!input.isEmpty() && !input.endsWith(EXT)) input += EXT;
-            return input;
-        }
-
-        @Override
-        public boolean checkInput(String inputString) {
-            return super.checkInput(forceExtension(inputString));
-        }
-
-        @Override
-        public boolean canClose(String inputString) {
-            return super.canClose(forceExtension(inputString));
-        }
-    }
-
-    @NotNull
+    @Nullable
     @Override
-    protected PsiElement[] invokeDialog(final Project project, PsiDirectory directory) {
-        Validator validator = new Validator(project, directory);
-        Messages.showInputDialog(
-                project,
-                IdeBundle.message("prompt.enter.new.file.name"),
-                IdeBundle.message("title.new.file"),
-                null, null, validator
-        );
-        return validator.getCreatedElements();
+    protected String getDefaultExtension() {
+        return CustomFileType.INSTANCE.getDefaultExtension();
     }
 }
