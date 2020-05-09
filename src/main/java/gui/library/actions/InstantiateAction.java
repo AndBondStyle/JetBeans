18117026035da7c5b2fbab60306652e6a2f7aa2a
AndBondStyle <andbondstyle@gmail.com>
2020-05-09 15:40:37 +0300

    Instantiate from shell action

--- src/main/java/gui/library/actions/InstantiateAction.java
+++ src/main/java/gui/library/actions/InstantiateAction.java
@@ -1,6 +1,7 @@
 package gui.library.actions;
 
 import com.intellij.openapi.project.DumbAware;
+import gui.common.ShellInputDialog;
 import gui.common.tree.PatchedNode;
 import gui.library.LibraryView;
 import core.JetBeans;
@@ -9,6 +10,7 @@ import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.icons.AllIcons;
 import com.intellij.openapi.project.Project;
+import gui.wrapper.Wrapper;
 import org.jetbrains.annotations.NotNull;
 
 import javax.swing.tree.TreePath;
@@ -22,8 +24,19 @@ public class InstantiateAction extends AnAction implements DumbAware {
     public void actionPerformed(@NotNull AnActionEvent e) {
         String klass = this.getClassID(e.getProject());
         if (klass == null) return;
+        ShellInputDialog dialog = new ShellInputDialog(e.getProject(), "Shell Input - New Instance");
+        dialog.evaluator.setReturnType(Object.class);
+        dialog.evaluator.setBody("\nreturn new " + klass + "();\n");
+        dialog.offset = 3;
         JetBeans core = JetBeans.getInstance(e.getProject());
-        core.instantiate(klass);
+        dialog.callback = () -> {
+            Wrapper wrapper = Wrapper.autowrap(dialog.result);
+            core.getCanvas().addItem(wrapper);
+            core.getCanvas().setSelection(wrapper);
+            core.fireEvent("instantiate");
+        };
+        dialog.init();
+        dialog.show();
     }
 
     @Override
